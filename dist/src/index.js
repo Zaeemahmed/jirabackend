91f6cbae583ef96dfdcfbbb703338c76574e78f9
Zaeemahmed <Zahmed.bscs18seecs@seecs.edu.pk>
2022-12-02 06:07:38 -0800

    deploy conifgs

---
+++ dist/src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.server = void 0;
+const apollo_server_1 = require("apollo-server");
+const apollo_server_core_1 = require("apollo-server-core");
+// 1
+const schema_1 = require("./schema");
+const context_1 = require("./context");
+exports.server = new apollo_server_1.ApolloServer({
+    schema: schema_1.schema,
+    context: context_1.context,
+    plugins: [apollo_server_core_1.ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+const port = 4000;
+exports.server.listen({ port }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
+//# sourceMappingURL=index.js.map
