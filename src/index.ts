0b672699030d64390654a68c25af2673de229862
Zaeemahmed <Zahmed.bscs18seecs@seecs.edu.pk>
2022-12-05 00:37:39 -0800

    cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import cors from "cors";
 
-// 1
 import { schema } from "./schema";
 import { context } from "./context";
 
@@ -11,6 +11,7 @@ export const server = new ApolloServer({
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   introspection: true,
   csrfPrevention: true,
+  cors: { origin: "https://jirabackend.vercel.app/", credentials: true },
 });
 
 const port = 4000;
